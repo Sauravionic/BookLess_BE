9e259cb727bb59701fad576075fadbd33d9a6210
Saurav Srivastava <srivastava2000saurav@gmail.com>
2022-02-27 22:49:02 +0530

    Changed Exception
    Exception Handling of email unique constraint

--- src/main/java/io/bookless/BookLess/Controller/SignUpController.java
+++ src/main/java/io/bookless/BookLess/Controller/SignUpController.java
@@ -2,18 +2,23 @@ package io.bookless.BookLess.Controller;
 
 import io.bookless.BookLess.DTO.UserRegistrationDTO;
 import io.bookless.BookLess.Service.UserService;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @Controller
 @RequestMapping("/signup")
 public class SignUpController {
 
     private UserService userService;
-
+    private boolean emailError = false;
     public SignUpController(UserService userService) {
         super();
         this.userService = userService;
@@ -25,13 +30,21 @@ public class SignUpController {
     }
 
     @GetMapping
-    public String signUp(){
+    public String signUp(Model model){
+        model.addAttribute("emailExists", " ");
         return "signup";
     }
 
     @PostMapping
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDTO registrationDto) {
-        userService.save(registrationDto);
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDTO registrationDto, Model model) {
+        try {
+            userService.save(registrationDto);
+        }
+        catch(DataIntegrityViolationException e) {
+            model.addAttribute("emailExists", "User already Exists !!!");
+            emailError = true;
+            return "signup";
+        }
         return "redirect:/regsucess";
     }
 
